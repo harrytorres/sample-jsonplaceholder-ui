9bce4bd69fbcca1ce3bd43d2b6597e945fb480b5
harryman2 <torresharry2@gmail.com>
2022-07-28 21:44:36 +0800

    update

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -123,22 +123,21 @@ class User extends React.Component{
               <div>
                 <div className="-mt-px flex divide-x divide-gray-200">
                   <div className="w-0 flex-1 flex">
-                    <a
+                    <div
                       href={`mailto:${person.email}`}
                       className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
                     >
                       <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
                       <span className="ml-3">Email</span>
-                    </a>
+                    </div>
                   </div>
                   <div className="-ml-px w-0 flex-1 flex">
-                    <a
-                      href={`tel:${person.phone}`}
+                    <div
                       className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
                     >
                       <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
                       <span className="ml-3">Call</span>
-                    </a>
+                    </div>
                   </div>
                 </div>
               </div>
